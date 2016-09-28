3fa5dfd656ef3637d3e883ce51920fd1a7af56a9
Ms. Bhavana DK <durugappa@qwinix.io>
2016-09-28 16:55:22 +0530

    IO and tdd

---
+++ fileio.rb
@@ -0,0 +1,33 @@
+puts "hello"
+puts "qwinix"
+putc "world"
+print "a"
+
+a=gets 
+puts "#{a} bhavana "
+
+File.new("w.txt","w")
+File.open("w.txt","r")
+File.read("w.txt")
+ "hello world"
+
+
+File.open("w.txt","w") do |f|
+var=f.gets
+puts "qwinix"
+end
+
+File.open("w.txt","w") do |f|
+var=f.gets
+puts "qwinix"
+end
+hello world
+=> nil
+
+
+ file = File.new("w.txt", "w+")
+ file.syswrite("Hello")
+5
+
+file.syswrite("Hello")
+"Hel"
